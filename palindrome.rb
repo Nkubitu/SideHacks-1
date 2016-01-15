df3747392a9766fa6b3c6bb50020503dbf8992d6
Nkubitu <muthonielaine@gmail.com>
2016-01-15 18:36:20 -0500

    Update palindrome.rb

--- palindrome.rb
+++ palindrome.rb
@@ -5,3 +5,16 @@ output.
 
 Authors: Mababio, M'Nkubitu, Ndemo
 =end
+puts "Enter string to test"=begin
+str = gets
+def isPalindrome 
+size = str.length
+size < 2? 'Palindrome': 'Not Palindrome'
+str.downcase
+str.gsub!(/\s/,'')
+str.gsub!(/[^0-9a-z]/i,'')
+str2 = str.reverse
+if str.eql? str2 = true
+  puts "Palindrome"
+else puts "Not Palindrome"
+end
