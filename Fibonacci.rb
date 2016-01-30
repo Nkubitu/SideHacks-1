76324665c4851df1dbe077b96b1d9c14f9114178
Callistus Ndemo <ndemocarlis@users.noreply.github.com>
2016-01-29 23:29:30 -0500

    Clarified the question to the Nth number.

--- Fibonacci.rb
+++ Fibonacci.rb
@@ -1,6 +1,6 @@
 =begin
 Fibonacci Sequence - Enter a number and have the program generate
-the Fibonacci sequence to that number or to the Nth number.
+the Fibonacci sequence to the Nth number.
 
 Authors: Mababio, M'Nkubitu, Ndemo
 
